04425335b13129a84e429f090f3e15f7386ca198
Facts <81759979+Faacts@users.noreply.github.com>
2022-11-08 21:56:33 +0100

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,6 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Faacts/FactsHub/main/Loader'))();
-wait()
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Use the new loadstring",
-	Text = "Join the discord to find it! (In Credits tab)",
-})
